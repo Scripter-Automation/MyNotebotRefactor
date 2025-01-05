a71f6d649613c3a09c69502c650e78321304aad2
Scripter-Automation <gscriptautomator@gmail.com>
2025-01-05 13:31:30 -0600

    firebase bug

--- src/lib/firebaseService.ts
+++ src/lib/firebaseService.ts
@@ -28,15 +28,15 @@ export default class FirebaseService{
             })
         }else{
             console.log("Initializing in production")
-            console.log(process.env.NODE_ENV)
+            console.log(process.env.PUBLIC_FIREBASE_API_KEY)
             this.app = initializeApp({
-                apiKey: process.env.FIREBASE_API_KEY,
-                authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-                projectId: process.env.FIREBASE_PROJECT_ID,
-                storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-                messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-                appId: process.env.FIREBASE_APP_ID,
-                measurementId: process.env.FIREBASE_MEASUREMENT_ID 
+                apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
+                authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+                projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
+                storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+                messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+                appId: process.env.PUBLIC_FIREBASE_APP_ID,
+                measurementId: process.env.PUBLIC_FIREBASE_MEASUREMENT_ID 
             });
             console.log("firebase service initialized")
         }
